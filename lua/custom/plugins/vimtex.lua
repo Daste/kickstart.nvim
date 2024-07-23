09ed0d97bc84aacc1744bc7e7e3a9ffddf28645d
Daste <Daste@users.noreply.github.com>
2024-07-23 18:36:06 +0200

    improved biblatex integration

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -7,6 +7,8 @@ return {
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_general_viewer = 'SumatraPDF'
       vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+      vim.g.Tex_DefaultTargetFormat = 'pdf'
+      vim.g.tex_flavor = 'latex'
     end,
   },
 }
