37b5f5b313d44448092cf2e070e0bfb03e7a8c4d
Dario Sterzi <sterzi.dario@gmail.com>
2024-08-04 12:40:05 +0200

    fixed luasnip mess; previous commit should not be considered independent

--- lua/custom/plugins/LuaSnip.lua
+++ lua/custom/plugins/LuaSnip.lua
@@ -1,37 +1,4 @@
-return {
-  {
-
-    'L3MON4D3/LuaSnip',
-    -- follow latest release.
-    version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = 'make install_jsregexp',
-    config = function()
-      require('luasnip').config.setup {
-        history = true,
-        updateevents = 'TextChanged,TextChangedI',
-        --        store_selection_keys = '<Tab>',
-      }
-      ---[[
-      -- inoremap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
-      vim.cmd [[
-        inoremap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-        snoremap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>
-        inoremap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-        snoremap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-      ]]
-      --]] a vimscript comment begins with
-      --FIX: here the keymaps clash! find how to fix it.
-
-      --vim.api.nvim_set_keymap('i', '<Tab>', '<cmd>lua require("luasnip").expand_or_jump()<CR>', { noremap = false, silent = true })
-      --vim.api.nvim_set_keymap('i', '<Tab>', '<Plug>luasnip-next-choice', { noremap = true, silent = true })
-      --vim.api.nvim_set_keymap('s', '<Tab>', '<Plug>luasnip-next-choice', {})
-      --vim.api.nvim_set_keymap('i', '<S-Tab>', '<Plug>luasnip-prev-choice', {})
-      --vim.api.nvim_set_keymap('s', '<S-Tab>', '<Plug>luasnip-prev-choice', {})
-
-      local config_dir = vim.fn.stdpath 'config'
-      local snippets_dir = config_dir .. '/lua/custom/plugins/LuaSnip/'
-      require('luasnip.loaders.from_lua').load { paths = { snippets_dir } }
-    end,
-  },
-}
+local config_dir = vim.fn.stdpath 'config'
+local snippets_dir = config_dir .. '/lua/custom/plugins/LuaSnip/'
+require('luasnip.loaders.from_lua').load { paths = { snippets_dir } }
+return {}
--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -5,8 +5,16 @@ return {
     -- tag = "v2.15", -- uncomment to pin to a specific release
     init = function()
       -- VimTeX configuration goes here, e.g.
+      --WARN:: some options depend on the machine I'm using, and should sit in a not commited file
+      -- things like the pdf viewer.
+      -- I have put it in lua/machine-specific/pdf-viewer.lua
+
+      --[[ 
+      -- TODO: move the following to a lua/machine-specific/pdf-viewer.lua on windows machines
+
       vim.g.vimtex_view_general_viewer = 'SumatraPDF'
       vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+      --]]
       vim.g.Tex_DefaultTargetFormat = 'pdf'
       vim.g.tex_flavor = 'latex'
     end,
